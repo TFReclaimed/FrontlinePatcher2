fda7fa0283b166afaee1c9c730e168706be9f7b9
Nuutti <snuutti@protonmail.com>
2026-04-11 22:09:11 +0300

    Build assetbundles in workflow

--- Overrides/Assets/Scripts/Editor/AssetTools.cs
+++ Overrides/Assets/Scripts/Editor/AssetTools.cs
@@ -1,9 +1,11 @@
+using System;
 using UnityEditor;
+using UnityEngine;
 
 public static class AssetTools
 {
     [MenuItem("Assets/TF Reclaimed/Build AssetBundles")]
-    private static void BuildAllAssetBundles()
+    private static void BuildAssetBundles()
     {
         EditorWindow.GetWindow<AssetBundleBuilderWindow>(true, "Build AssetBundles", true);
     }
@@ -13,5 +15,30 @@ public static class AssetTools
     {
         AssetDatabase.ForceReserializeAssets();
         AssetDatabase.SaveAssets();
+    }
+
+    public static void BuildAssetBundlesCi()
+    {
+        var args = Environment.GetCommandLineArgs();
+        string assetBundlePath = null;
+        for (var i = 0; i < args.Length; i++)
+        {
+            if (args[i] == "-assetbundlePath")
+            {
+                assetBundlePath = args[i + 1];
+                break;
+            }
+        }
+
+        if (string.IsNullOrEmpty(assetBundlePath))
+        {
+            Debug.LogError("No asset bundle path provided!");
+            return;
+        }
+
+        BuildPipeline.BuildAssetBundles(assetBundlePath, BuildAssetBundleOptions.None,
+            EditorUserBuildSettings.activeBuildTarget);
+
+        Debug.Log("Asset bundles successfully built at: " + assetBundlePath);
     }
 }
